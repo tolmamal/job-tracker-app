9d892d27ead1f61e39f759f74eb70e9fe35561b4
tolmamal <tolmamal@gmail.com>
2020-10-27 16:44:31 +0200

    table

--- src/containers/AnalysisTable/AnalysisTable.js
+++ src/containers/AnalysisTable/AnalysisTable.js
@@ -151,11 +151,11 @@ class MuiVirtualizedTable extends React.PureComponent {
 const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
 const sample = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
+    ['17/05/20', 5, 0, 0, 0, 0],
+    ['16/05/20', 7, 0, 0, 0, 0],
+    ['14/05/20', 5, 0, 0, 0, 0],
+    ['12/05/20', 7, 0, 0, 0, 0],
+    ['11/05/20', 5, 0, 0, 0, 0],
 ];
 
 const createData = (date, applied, inprogress, interview, rejected, accepted) => {
