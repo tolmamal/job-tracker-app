430e3f337b917755752129f230712be412f2842f
tolmamal <tolmamal@gmail.com>
2020-10-27 12:05:40 +0200

    dates range

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -214,10 +214,12 @@ const Dashboard = () => {
         const maxDate = endDate.setHours(23,59,0);
         const range = moment.range(minDate, maxDate);
 
-
+        console.log("minDate: " + minDate);
+        console.log("maxDate: " + maxDate);
 
         applications.map((item) => {
             const currentDate = moment(item.time, "DD/MM/YYYY").toDate();
+            console.log("item.time:  " + item.time + "  currentDate: " + currentDate);
             if (range.contains(currentDate)) {
                 console.log("*** range MATCH!   item.title: " + item.title + "   ****");
                 itemsInRange.push(item);
